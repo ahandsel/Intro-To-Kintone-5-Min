8b8a2b8ebb5d9e19d1f8624f5d81d5e97146d75c
Genji Fujimori <genji-fujimori@cybozu.co.jp>
2022-12-11 22:14:48 +0900

    Update windi.config.ts

--- intro-to-kintone-5-min-slides/windi.config.ts
+++ intro-to-kintone-5-min-slides/windi.config.ts
@@ -1,19 +1,17 @@
-import { resolve } from 'path'
-import { mergeWindicssConfig, defineConfig } from 'vite-plugin-windicss'
-import BaseConfig from '@slidev/client/windi.confisg'
+import { resolve } from "path";
+import { mergeWindicssConfig, defineConfig } from "vite-plugin-windicss";
+import BaseConfig from "@slidev/client/windi.config";
 
 // extend the base config
 export default mergeWindicssConfig(
   BaseConfig,
   defineConfig({
     extract: {
-      include: [
-        resolve(__dirname, '**/*.{vue,ts}'),
-      ],
+      include: [resolve(__dirname, "**/*.{vue,ts}")],
     },
     shortcuts: {
       // custom the default background
-      'bg-main': 'bg-white text-[#181818] dark:(bg-[#121212] text-[#ddd])',
+      "bg-main": "bg-white text-[#181818] dark:(bg-[#121212] text-[#ddd])",
     },
     theme: {
       extend: {
@@ -29,12 +27,11 @@ export default mergeWindicssConfig(
           },
         },
         // fonts can be replaced here, remember to update the web font links in `index.html`
-        // fontFamily: {
-          // sans: '"Nunito","Nunito Sans", ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
-          // sans: '"Poppins","Nunito Sans", ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
-          // mono: '"Fira Code", monospace',
-        // },
+        fontFamily: {
+          sans: '"Poppins","Nunito Sans", ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
+          mono: '"Fira Code", monospace',
+        },
       },
     },
-  }),
-)
+  })
+);
